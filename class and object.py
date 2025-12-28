5786663ad0a124122b8b3728ecdbffd2a20ba1d8
Gaurav Gocher <gujjargaurav712@gmail.com>
2025-12-28 23:21:46 +0530

    Add files via upload

---
+++ class and object.py
@@ -0,0 +1,13 @@
+#housedesign is a class
+class housedesign:
+    color="red"
+    price=10000
+
+#object=class()
+gaurav_home=housedesign()
+print(gaurav_home,gaurav_home.color)
+
+h1=housedesign()
+print(h1.color)
+h1.color="blue"
+print("new color :",h1.color)
