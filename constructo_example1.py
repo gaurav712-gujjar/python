26e9fa2c08b6f707ef263e642f9183622e4eca0d
Gaurav Gocher <gujjargaurav712@gmail.com>
2025-12-29 22:30:07 +0530

    Add files via upload

---
+++ constructo_example1.py
@@ -0,0 +1,14 @@
+class valuecounts:
+    a=0
+    def __init__(self):
+        valuecounts.a=valuecounts.a+1
+
+
+v1=valuecounts()
+print(v1.a)
+
+v2=valuecounts()
+print(v2.a)
+
+v3=valuecounts()
+print(v3.a)
